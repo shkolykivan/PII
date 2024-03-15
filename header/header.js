2869a7b8ec4165438bc6cb92a8b1cf78850d02b2
shkolykivan <148362331+shkolykivan@users.noreply.github.com>
2024-03-15 12:37:30 +0200

    Update header.js

--- header/header.js
+++ header/header.js
@@ -1,16 +1,11 @@
+document.querySelector('.header').addEventListener('click', () => {
+    document.querySelector('.modal__account').style.display = 'none'
+    document.querySelector('.modal__masage').style.display = 'none'
 
-document.querySelector('.header__account').addEventListener('click', () => {
-    document.querySelector('.modal__account').style.display = 'block'
-})
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('.header__account'))
-        document.querySelector('.modal__account').style.display = 'none'
+    if (event.target.closest('.header__account')){
+        document.querySelector('.modal__account').style.display = 'block'
+    }else if(event.target.closest('.header__massage')){
+        document.querySelector('.modal__masage').style.display = 'block'
+    }
 })
 
-document.querySelector('.header__massage').addEventListener('click', () => {
-    document.querySelector('.modal__masage').style.display = 'block'
-})
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('.header__massage'))
-        document.querySelector('.modal__masage').style.display = 'none'
-})
